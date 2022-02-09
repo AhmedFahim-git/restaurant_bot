8fdfc95d810ddc7e38d141c99983481a904e3326
Ahmed Fahim <74918992+AhmedFahim-git@users.noreply.github.com>
2022-02-09 21:18:19 +0600

    Added comments

--- actions/actions.py
+++ actions/actions.py
@@ -21,6 +21,7 @@ from word2number import w2n
 geolocator = Nominatim(user_agent="rasa_chat")
 reverse = RateLimiter(geolocator.reverse, min_delay_seconds=0.1)
 API_KEY = dotenv_values()["API_KEY"]
+
 class ActionBeginningSearch(Action):
     def name(self) -> Text:
         return "action_beginning_search"
@@ -29,12 +30,15 @@ class ActionBeginningSearch(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
+        # Utter message before making search
         dispatcher.utter_message(text=f'Looking for {tracker.get_slot("place_type")} in \
             {", ".join(tracker.get_slot("address"))} with a search radius of {tracker.get_slot("radius")} km')
         return []
 
+
 class ActionPlacesSearch(Action):
     def __init__(self) -> None:
+        # Defining dictionary for mapping to category ids
         self.code_dict = {'restaurants':'13065', 'coffee houses':'13032', 'both restaurants and coffee houses':'13065,13032'}
     
     def name(self) -> Text:
@@ -44,38 +48,47 @@ class ActionPlacesSearch(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
+        # Getting parameter values from slots
         categories = self.code_dict[tracker.get_slot('place_type')]
         location = tracker.get_slot('lat_lon')
         radius = int(float(tracker.get_slot('radius'))*1000)
         
+        # Setting headers to pass to the API call
         headers = {
             "Accept": "application/json",
             "Authorization": API_KEY
         }
-
+        
         params = {'ll':location, 'radius':radius, 'categories':categories}
         url = 'https://api.foursquare.com/v3/places/search'
         
         try:
+            # Making the API call
             r = requests.get(url, params=params, headers=headers)
-
+            
+            # Parsing the returned json to get desired values
             results = {'name':[], 'lat':[], 'lon':[], 'distance':[]}
+            
             for result in r.json()['results']:
                 results['name'].append(result['name'])
                 results['lat'].append(result['geocodes']['main']['latitude'])
                 results['lon'].append(result['geocodes']['main']['longitude'])
                 results['distance'].append(result['distance'] / 1000)
-
+            
             df = pd.DataFrame(results)
-
+            
+            # We get the latitude and values from the FOURSQUARE API, and we convert them to addresses by reverse geocoding using geopy Nominatim
             df['address'] = df.apply(lambda x: reverse((x['lat'], x['lon']) ).address , axis=1)
             
+            # Format the dataframe to bot response
             bot_response = '\n\n'.join(df.apply(lambda x: f"Name: {x['name']}\nAddress: {x['address']}\nDistance: {x['distance']} km", axis=1))
         except:
+            # In case we run into an error we return this response
             bot_response = 'No results returned. Try with a different location or larger radius.'
         
         dispatcher.utter_message(text = bot_response)
         return []
+
 
 class ValidatePredefinedSlots(ValidationAction):
     def validate_address(
@@ -85,15 +98,20 @@ class ValidatePredefinedSlots(ValidationAction):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
+        
         try:
+            # Get the latitude and longitude of the address by geocoding using geopy Nominatim
             location = geolocator.geocode(', '.join(slot_value))
             
             if location is None:
+                # If the location isn't recognized by geopy Nominatim we return the following message and set the slot value to none
                 dispatcher.utter_message(template="utter_wrong_address")
                 return {"address": None}
             else:
+                # If all is right we keep the address slot value as is. We also set the value of the lat_lon slot using the latitude and longitude value of the current address
                 return {"address": slot_value, 'lat_lon': f'{location.latitude},{location.longitude}'}
         except:
+            # In case we run into an error we return the following message
             dispatcher.utter_message(text='Facing server issues. Please try again later')
 
     def validate_radius(
@@ -103,8 +121,12 @@ class ValidatePredefinedSlots(ValidationAction):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
+        
         try:
+            # if the user gave a valid number as input we should be able to convert to numeric form
             number =  int(float(w2n.word_to_num(slot_value))*1000 )
+            
+            # The radius should be between 0 to 100 km
             if number > 100000:
                 dispatcher.utter_message(text='Maximum radius is 100 km. Setting radius to 100 km.')
                 return {'radius': str(100)}
@@ -114,6 +136,7 @@ class ValidatePredefinedSlots(ValidationAction):
             else:
                 return {'radius': str(number/1000)}
         except:
+            # if user gave input that cannot be converted into a number, then we return the following messsage
             dispatcher.utter_message(template="utter_wrong_radius")
             return {"radius": None}
 
@@ -128,15 +151,20 @@ class ValidatePlacesSearchForm(FormValidationAction):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
+        
         try:
+            # Get the latitude and longitude of the address by geocoding using geopy Nominatim
             location = geolocator.geocode(', '.join(slot_value))
-            dispatcher.utter_message(text = f'{slot_value}')
+            
             if location is None:
+                # If the location isn't recognized by geopy Nominatim we return the following message and set the slot value to none
                 dispatcher.utter_message(template="utter_wrong_address")
                 return {"address": None}
             else:
+                # If all is right we keep the address slot value as is. We also set the value of the lat_lon slot using the latitude and longitude value of the current address
                 return {"address": slot_value, 'lat_lon': f'{location.latitude},{location.longitude}'}
         except:
+            # In case we run into an error we return the following message
             dispatcher.utter_message(text='Facing server issues. Please try again later')
     
     def validate_radius(
@@ -146,8 +174,12 @@ class ValidatePlacesSearchForm(FormValidationAction):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
+        
         try:
+            # if the user gave a valid number as input we should be able to convert to numeric form
             number =  int(float(w2n.word_to_num(slot_value))*1000 )
+            
+            # The radius should be between 0 to 100 km
             if number > 100000:
                 dispatcher.utter_message(text='Maximum radius is 100 km. Setting radius to 100 km.')
                 return {'radius': str(100)}
@@ -157,6 +189,7 @@ class ValidatePlacesSearchForm(FormValidationAction):
             else:
                 return {'radius': str(number/1000)}
         except:
+            # if user gave input that cannot be converted into a number, then we return the following messsage
             dispatcher.utter_message(template="utter_wrong_radius")
             return {"radius": None}
 
